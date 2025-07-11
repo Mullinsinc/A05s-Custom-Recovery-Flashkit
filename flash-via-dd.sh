92cd36a9654a5a251b0d2c997fb5194e4fddc6f7
Junaid Mullins <junaidmullins49@gmail.com>
2025-07-11 22:06:57 +0200

    Create flash-via-dd.sh
    A simple Termux-friendly script to flash recovery.img and vbmeta.img via dd

---
+++ flash-via-dd.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+
+# Mount point: /sdcard/a05s_bundle
+cd /sdcard/a05s_bundle || exit 1
+
+echo "[*] Flashing recovery..."
+su -c "dd if=recovery.img of=/dev/block/by-name/recovery bs=4096"
+echo "[+] Recovery flashed."
+
+echo "[*] Flashing vbmeta (disable AVB)..."
+su -c "dd if=vbmeta.img of=/dev/block/by-name/vbmeta bs=4096"
+echo "[+] vbmeta flashed."
+
+echo "[✓] Done. You can now reboot into recovery!"
